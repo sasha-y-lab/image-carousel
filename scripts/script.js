6cf9a3fb2c76253020461b2520d0d3ac28afc747
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-11-27 11:51:03 -0500

    Slides navigation works now

--- scripts/script.js
+++ scripts/script.js
@@ -23,20 +23,28 @@ const img3 = document.querySelector(".image-3");
 // slider buttons next and previous needs to push to each 
 // photo, 1, 2, 3 or forward then backward
 
+const circlesDiv = document.querySelectorAll(".circle-divs");
+
+const circle1 = document.querySelector(".img-1");
+const circle2 = document.querySelector(".img-2");
+const circle3 = document.querySelector(".img-3");
+
 const nextBtn = document.querySelector("#next");
 
 const previousBtn = document.querySelector("#previous");
 
 const imagesArray = [img1, img2, img3];
 
-const STEP = 340;
+const STEP = 50;
 
-const STEP2 = -340;
+const STEP2 = -50;
 
 let x = 0;
 
 // there must be an easier way to switch forward then backward
-// without needing exact numbers
+// without needing exact numbers - done
+
+
 
 previousBtn.addEventListener("mousedown", (e) => {
 
@@ -58,28 +66,6 @@ previousBtn.addEventListener("mousedown", (e) => {
 
 
 
- // if (positionX === 3) {
-  // slider.style.transform = "translateX(50px)";
-  // positionX = 4;
-  // console.log(positionX);
-  //}
-
-
-  //requestAnimationFrame(animateMovement);
-    //slider.style.transform = "translateX(-316px)";
-    
-     //slider.style.transform = "translate(50px)";
-    //img1.style.transform = "translate(300px)";
-  //  img2.style.transform = "translate(300px)";
-   // img3.style.transform = "translate(300px)";
-
- //}
-//animateMovement(slider);
-
-
-
-
-
 nextBtn.addEventListener("mousedown", (e) => {
 
     console.log(e.target); // yup it is clicked
@@ -93,3 +79,84 @@ nextBtn.addEventListener("mousedown", (e) => {
   slider.style.transform = `translateX(${x}px)`;
 
 });
+
+
+// Add in some navigation dots at the bottom of the slides. - done
+// Make a horizontal series of empty circles with CSS immediately 
+// below the slideshow. Each circle represents a slide, so whenever
+//  a new slide is activated, its corresponding circle gets filled 
+// in so you can tell where in the show you are. Make each circle 
+// link to that particular slide, so you can click on the circle and
+//  it will jump to that slide.
+
+//circlesDiv.forEach((circle, index) => {
+
+  //let index = 0;
+
+  //x = 340;
+
+/* circle1.addEventListener("mousedown", (e) => {
+
+
+
+ console.log(e.target);
+// if click on circle one, show picture 1 - how do i show picture one
+// slider needs to move to pic 1
+slider.style.transform = `translateX(${x}px)`;
+
+
+
+});
+
+
+circle2.addEventListener("mousedown", (e) => {
+
+x = -5;
+
+ console.log(e.target);
+// if click on circle one, show picture 1 - how do i show picture one
+// slider needs to move to pic 1
+slider.style.transform = `translateX(${x}px)`;
+
+
+
+});
+
+circle3.addEventListener("mousedown", (e) => {
+
+  x = -40;
+
+
+ console.log(e.target);
+// if click on circle one, show picture 1 - how do i show picture one
+// slider needs to move to pic 1
+slider.style.transform = `translateX(${x}px)`;
+
+
+
+});
+
+
+*/
+
+// ---- CIRCLES ---- //
+circle1.addEventListener("mousedown", (e) => {
+  console.log(e.target);
+  x = 340;  // center first image
+  slider.style.transform = `translateX(${x}px)`;
+});
+
+circle2.addEventListener("mousedown", (e) => {
+  console.log(e.target);
+  x = -5;  // center second image
+  slider.style.transform = `translateX(${x}px)`;
+});
+
+circle3.addEventListener("mousedown", (e) => {
+  console.log(e.target);
+  x = -315;  // center third image
+  slider.style.transform = `translateX(${x}px)`;
+});
+  
+// goes from side to side and clickable on navigation buttons
+
